ac4403a9de4e4118e14701ce5686d7e84c7b56c0
jribacruz <jribacruz@gmail.com>
2017-03-30 21:26:15 -0300

    detecção de face com video

--- face-detection-image.py
+++ face-detection-image.py
@@ -5,14 +5,11 @@ face_cascade = cv2.CascadeClassifier('/usr/share/OpenCV/haarcascades/haarcascade
 eye_cascade = cv2.CascadeClassifier('/usr/share/OpenCV/haarcascades/haarcascade_eye.xml')
 
 
-img = cv2.imread('/home/jcruz/Imagens/fotos/eu.jpg')
+img = cv2.imread('/home/jcruz/Imagens/fotos/amor.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-print(faces)
 for (x, y, w, h) in faces:
-    # Na documentação https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_objdetect/py_face_detection/py_face_detection.html#face-detection
-    # está atribuindo a variável img
-    # removido para funcionar
+    #atribuicao para img removido para funcionar
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
     roi_gray = gray[y:y + h, x:x + w]
     roi_color = img[y:y + h, x:x + w]
